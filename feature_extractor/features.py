bc1fe90f3ea555568c4883625521158991f9828e
gabrielopesantos <gabrielopesantos97@gmail.com>
2021-06-13 17:47:19 +0100

    Statistical and time features reworked/added

--- feature_extractor/features.py
+++ feature_extractor/features.py
@@ -5,139 +5,184 @@ import scipy.stats
 
 from feature_extractor.utils import set_domain, compute_timestamps_array
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+# Statistical features
+
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_mean(signal):
     """
-    expects a 1d numpy array
-    returns the mean of the array
+    Mean of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.mean(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_median(signal):
     """
-    expects a 1d numpy array
-    returns the median of the array
+    Median of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.median(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_max(signal):
     """
-    expects a 1d numpy array
-    returns the max value of the array
+    Max of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.max(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_min(signal):
     """
-    expects a 1d numpy array
-    returns the min value of the array
+    Min of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.min(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_std(signal):
     """
-    expects a 1d numpy array
-    returns the standard deviation of the array
+    Standard deviation of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.std(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_variance(signal):
     """
-    expects a 1d numpy array
-    returns the variance of the array
+    Variance of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.var(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_range(signal):
     """
-    expects a 1d numpy array
-    returns range between the max and min of the array
+    Range between max and min of a 1-dimensional array
+    Expects a 1d numpy array
     """
-    return get_max(signal) - get_min(signal)
+
+    return np.max(signal) - np.min(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_iqr(signal):
     """
-    expects a 1d numpy array
-    returns the intra quartile range between 25 and 75 (default values) of the array.
+    Intra quartile range of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return sp.stats.iqr(signal)
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
+@set_domain(domain=["statistical"], input=["1d array"])
+def get_kurtosis(signal):
+    """
+    Kurtosis of a 1-dimensional array
+    Expects a 1d numpy array
+    """
+
+    return sp.stats.kurtosis(signal)
+
+
+@set_domain(domain=["statistical"], input=["1d array"])
+def get_skewness(signal):
+    """
+    Kurtosis of a 1-dimensional array
+    Expects a 1d numpy array
+    """
+
+    return sp.stats.skew(signal)
+
+
+@set_domain(domain=["statistical"], input=["1d array"])
 def get_rms(signal):
     """
-    expects a 1d numpy array
-    returns the root mean squared "value" of the array.
+    Root mean square of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.sqrt(np.sum(signal ** 2) / len(signal))
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
-def get_energy(signal):
+@set_domain(domain=["statistical"], input=["1d array"])
+def get_mean_abs_deviation(signal):
     """
-    expects a 1d numpy array
-    returns the "energy" of the array.
+    Mean absolute deviation of a 1-dimensional array
+    Expects a 1d numpy array
     """
-    return np.sum(signal ** 2) / len(signal)
+
+    return np.mean(np.abs(signal - np.mean(signal)))
 
 
-@set_domain(domain=["statistical", "time"], input=["1d array"])
-def zero_crossings(signal):
+@set_domain(domain=["statistical"], input=["1d array"])
+def get_median_abs_deviation(signal):
     """
-    expects a 1d numpy array
-    returns the zero crossings of the array.
+    Median absolute deviation of a 1-dimensional array
+    Expects a 1d numpy array
     """
-    return float(len(np.where(np.diff(np.sign(signal)))))
 
+    return scipy.stats.median_absolute_deviation(signal, scale=1)
 
-#signal magnitude vector
-@set_domain(domain=["statistical", "time"], input=["2d array"])
-def get_smv(signal: np.array):
-    """
-    expects a 2d numpy array with the format: [samples, components]
-    """
-    x, y, z = np.split(signal, 3, axis=1)
-    return np.sqrt(np.sum(x ** 2) + np.sum(y ** 2) + np.sum(z ** 2))
+# Time domain features
 
-
-# TODO: Flatten
 @set_domain(domain=["time"], input=["1d array"])
 def get_autocorr(signal):
     """
-    expects a 1d numpy array 
-    computes the autocorrelation of the signal
+    Autocorrelation of a 1-dimensional array
+    Expects a 1d numpy array 
     """
+
     return float(np.correlate(signal, signal))
 
-# TODO: VER
-# @set_domain(domain=["time"], input=["1d array", "fs"])
-# def get_centroid(signal, fs):
-#     """
-#     expects a 1d numpy array
-#     computes the centroid along the time axis
-#     """
-#     pass
+
+"""
+@set_domain(domain=["time"], input=["2d array"])
+def get_crosscorr(signal):
+    pass
+"""
+
+@set_domain(domain=["time"], input=["1d array"])
+def zero_crossings(signal):
+    """
+    Zero-crossings of a 1-dimensional array
+    Expects a 1d numpy array
+    """
+    # Does not work with Z without subtracting 1
+
+    return float(len(np.where(np.diff(np.sign(signal)))))
+
+
+@set_domain(domain=["time"], input=["2d array"])
+def get_smv(signal):
+    """
+    Signal magnitude vector of a 2-dimensional array
+    Expects a 2d numpy array with the format: [samples, components]
+    """
+
+    x, y, z = np.split(signal, 3, axis=1)
+    return np.sqrt(np.sum(x ** 2) + np.sum(y ** 2) + np.sum(z ** 2))
 
 
 @set_domain(domain=["time"], input=["1d array"])
 def get_negative_turning_count(signal):
     """
-    expects a 1d numpy array
+    Number of negative turnings of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     signal_diff = np.diff(signal)
     signal_diff_idxs = np.arange(len(signal_diff[:-1]))
 
@@ -150,8 +195,10 @@ def get_negative_turning_count(signal):
 @set_domain(domain=["time"], input=["1d array"])
 def get_positive_turning_count(signal):
     """
-    expects a 1d numpy array
+    Number of positive turnings of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     signal_diff = np.diff(signal)
     signal_diff_idxs = np.arange(len(signal_diff[:-1]))
 
@@ -161,11 +208,34 @@ def get_positive_turning_count(signal):
     return len(positive_turning_pts)
 
 
+# TODO: VER
+@set_domain(domain=["time"], input=["1d array"])
+def get_energy(signal):
+    """
+    Energy of a 1-dimensional array
+    Expects a 1d numpy array
+    """
+
+    return np.sum(signal ** 2) / len(signal)
+
+
+# TODO: VER
+# @set_domain(domain=["time"], input=["1d array", "fs"])
+# def get_centroid(signal, fs):
+#     """
+#     expects a 1d numpy array
+#     computes the centroid along the time axis
+#     """
+#     pass
+
+
 @set_domain(domain=["time"], input=["1d array"])
 def get_mean_diff(signal):
     """
-    expects a 1d numpy array
+    Mean of the differences between values of a 1-dimensional array
+    Expects a 1d numpy array
     """
+
     return np.mean(np.diff(signal))
 
 
@@ -174,6 +244,7 @@ def get_median_abs_diff(signal):
     """
     expects a 1d numpy array
     """
+
     return np.median(np.abs(np.diff(signal)))
 
 
@@ -182,6 +253,7 @@ def get_median_diff(signal):
     """
     expects a 1d numpy array
     """
+    
     return np.median(np.diff(signal))
 
 
@@ -190,6 +262,7 @@ def get_distance(signal):
     """
     expects a 1d numpy array
     """
+
     signal_diff = np.sign(signal) 
     return np.sum([np.sqrt(1 + signal_diff ** 2)])
 
@@ -199,8 +272,9 @@ def get_sum_abs_diff(signal):
     """
     expects a 1d numpy array
     """
-    signal_diff = np.sign(signal) 
-    return np.sum(np.abs(np.diff(signal)))
+
+    signal_sign = np.sign(signal) 
+    return np.sum(np.abs(np.diff(signal_sign)))
 
 
 @set_domain(domain=["time"], input=["1d array", "fs"])
@@ -208,6 +282,7 @@ def get_total_energy(signal, fs):
     """
     expects a 1d numpy array
     """
+
     t = compute_timestamps_array(len(signal), fs)
 
     return np.sum(np.array(signal) ** 2) / (t[-1] - t[0])
@@ -227,6 +302,10 @@ def get_auc(signal, fs):
     """
     Expects a 1D numpy array
     """
+
     t = compute_timestamps_array(len(signal), fs)
 
     return np.sum(.5 * np.diff(t) * np.abs(signal[:-1] + signal[1:]))
+
+# Frequency domain features (Spectral domain?)
+#@set_domain(domain=["frequency"], input="1d array")
